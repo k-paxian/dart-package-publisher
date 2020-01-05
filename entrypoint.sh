162a13a7e00e08cf63879dd1b8c9947a963558f3
Alexander Mazuruk <leybniz@gmail.com>
2020-01-05 23:41:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,8 @@ switch_working_directory() {
 get_local_package_version() {
   pub get
   DEPS_OUT=`pub deps`
-  echo ">>>'$DEPS_OUT'"
+  PACKAGE_INFO=`echo "$DEPS_OUT" | cut -d\n -f3`
+  echo "$PACKAGE_INFO"
 }
 
 publish() {
