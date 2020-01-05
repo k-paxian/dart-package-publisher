08b4e0d9a0d74d6a5f22abb9eb1e4e685b97bdc9
Alexander Mazuruk <leybniz@gmail.com>
2020-01-05 22:24:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,37 @@
 #!/bin/sh -l
 
-echo "accessToken $1"
-echo "refreshToken $2"
-echo "package $3"
-echo "relativePath $4"
+ACCESS_TOKEN="$1"
+REFRESH_TOKEN="$2"
+PACKAGE="$3"
+RELATIVE_PATH="$4"
+
+check_inputs() {
+  echo "Check inputs..."
+  if [ -z "$ACCESS_TOKEN" ]; then
+    echo "Missing accessToken"
+    exit 1
+  fi
+  if [ -z "$REFRESH_TOKEN" ]; then
+    echo "Missing refreshToken"
+    exit 1
+  fi
+  if [ -z "$PACKAGE" ]; then
+    echo "Missing package name"
+    exit 1
+  fi
+  echo "OK"
+}
+
+switch_working_directory() {
+  echo "Switching to package directory '$RELATIVE_PATH'"
+  cd "$RELATIVE_PATH"
+}
+
+publish() {
+  echo "Publish dart package to Pub"
+  pub publish --dry-run
+  pub publish -f
+}
+
+check_inputs
+switch_working_directory
