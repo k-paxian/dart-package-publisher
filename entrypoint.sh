7254360b09651b55564ff7f3cf8609c26170e671
Alexander Mazuruk <leybniz@gmail.com>
2020-01-06 13:53:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ get_local_package_version() {
   pub get
   DEPS_OUT=`pub deps`
   PACKAGE_INFO=`echo "$DEPS_OUT" | cut -d\n -f3`
-  printf "%s\n" "$PACKAGE_INFO"
+  echo "$PACKAGE_INFO" | od -t x1
   IFS='\n'
   read -a parts <<< "$PACKAGE_INFO"
   for val in "${parts[@]}";
