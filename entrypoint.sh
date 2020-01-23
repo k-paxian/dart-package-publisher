a602e47f3e04936b9ebbef6f3d9861b4b8e1e2c7
Alexander Mazuruk <leybniz@gmail.com>
2020-01-23 12:40:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,14 +39,15 @@ get_local_package_version() {
     OUT=`pub deps`
   fi
 
-  PACKAGE_INFO=`echo "$OUT" | cut -d'|' -f1 | cut -d"'" -f1 | sed '/^\s*$/d'`
+  #PACKAGE_INFO=`echo "$OUT" | cut -d'|' -f1 | cut -d"'" -f1 | sed '/^\s*$/d'`
   #IFS=$'\n\r' read -d '' -r -a lines <<< "$PACKAGE_INFO"
   #lastIndex=`expr ${#lines[@]}-1`
-  echo "Package info:\n$PACKAGE_INFO"  
+  echo "out: $OUT"
   #PACKAGE_INFO="${lines[$lastIndex]}"
-  #PACKAGE=`echo "$PACKAGE_INFO" | cut -d' ' -f1`
   
-  LOCAL_PACKAGE_VERSION=`echo "$PACKAGE_INFO" | cut -d' ' -f2`
+  #PACKAGE=`echo "$PACKAGE_INFO" | cut -d' ' -f1`  
+  #LOCAL_PACKAGE_VERSION=`echo "$PACKAGE_INFO" | cut -d' ' -f2`
+  
   echo "Package: $PACKAGE"
   echo "Local version: [$LOCAL_PACKAGE_VERSION]"
   if [ -z "$PACKAGE" ]; then
